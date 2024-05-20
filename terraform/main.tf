82c8e2f329ba1273cdfba51711f1142ab7b065a8
VohnikMB <101456452+VohnikMB@users.noreply.github.com>
2024-05-20 17:48:34 +0300

    Lab 4-5
    forget commit

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+resource "virtualbox_vm" "my-vm" {
+count = 2 
+  name        = "my-vm-${count.index + 1}" 
+  image       = var.img
+  memory      = var.ram
+  cpus        = "1"
+  boot_order  = ["disk"]
+
+  network_adapter {
+       type = "bridged"
+       host_interface="en0"
+    }
+}
+
+resource "local_file" "tf_ip" { 
+content = "[ALL]\n${ virtualbox_vm.my-vm[0].network_adapter.0.ipv4_address} ansible_ssh_user=ubuntu" 
+  	filename = ". /inventory"
+}
